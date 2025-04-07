08aa62ca4dc386d9eb95632f4a0f46452cad265b
Angebriela <angelachiyal07@gmail.com>
2025-04-06 21:15:45 -0600

    Carga completa de clases

---
+++ ReporteArchivo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Repaso8Propiedades
+{
+    internal class ReporteArchivo
+    {
+        public void Guardar(string archivo, List<ReportePropiedad> reportes)
+        {
+            string json = JsonConvert.SerializeObject (reportes);
+            System.IO.File.WriteAllText(archivo, json);
+        }
+
+        public List<ReportePropiedad > Leer(string archivo)
+        {
+            List<ReportePropiedad > lista = new List<ReportePropiedad>();
+            StreamReader jsonStream = File.OpenText(archivo);
+            string json = jsonStream.ReadToEnd();
+            jsonStream.Close();
+            lista = JsonConvert.DeserializeObject<List<ReportePropiedad>>(json);
+            return lista;
+        }
+    }
+}
